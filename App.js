5970df022044758dc968b7c9f28e89b145cc7ca6
Dmitry1606 <81901638+Dmitry1606@users.noreply.github.com>
2022-11-07 20:26:23 +0300

    Update App.js

--- App.js
+++ App.js
@@ -1,4 +1,4 @@
-// import bg from '../../bg.png';
+import bg from './bg.png';
 import './styles.css';
 
 
@@ -13,10 +13,11 @@ function App() {
           <h3>Attractions</h3>
         </div>
         <h4>Discover stays to live, or just relax</h4>
-        {/* <img src={bg} className='bg.png' alt='bg'></img> */}
+        { <img src={bg} className='bg.png' alt='bg'></img> }
       </header>
     </div>
   );
 }
 
 export default App;
+
