5b966400cdd1021970223f7b60df68c98a3a3b58
Dmitry1606 <81901638+Dmitry1606@users.noreply.github.com>
2022-11-07 20:04:43 +0300

    Create App.js

---
+++ App.js
@@ -0,0 +1,22 @@
+// import bg from '../../bg.png';
+import './styles.css';
+
+
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <h1>Trip House</h1>
+        <div className='Title'>
+          <h2>Stays</h2>
+          <h3>Attractions</h3>
+        </div>
+        <h4>Discover stays to live, or just relax</h4>
+        {/* <img src={bg} className='bg.png' alt='bg'></img> */}
+      </header>
+    </div>
+  );
+}
+
+export default App;
