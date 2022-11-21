966ec027cea2c0cbc934bbd62d335a8c7210f14f
Dmitry1606 <81901638+Dmitry1606@users.noreply.github.com>
2022-11-21 15:18:57 +0300

    Update App.js

--- App.js
+++ App.js
@@ -1,22 +1,23 @@
 import bg from './bg.png';
 import './styles.css';
 
-
-
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <div style={{ backgroundImage: 'url(./bg.png)' }}>
-
-        </div>
+    <div className='App'>
+      <header className='App-header'>
+        <div
+        className='Background'
+          id='test'
+          style={{
+            backgroundImage: `url(${bg})`, 
+          }}
+        ></div>
         <h1>Trip House</h1>
         <div className='Title'>
           <h2>Stays</h2>
           <h3>Attractions</h3>
         </div>
         <h4>Discover stays to live, or just relax</h4>
-        {<img src={bg} className='bg.png' alt='bg'></img>}
       </header>
     </div>
   );
