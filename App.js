74b682cd85329d4404b8962fe78740fd03c3fbf2
Dmitry1606 <81901638+Dmitry1606@users.noreply.github.com>
2022-11-27 21:39:38 +0300

    Update App.js

--- App.js
+++ App.js
@@ -18,9 +18,16 @@ function App() {
           <h3>Attractions</h3>
         </div>
         <h4>Discover stays to live, or just relax</h4>
+        <div className='input'>
+          <input name='email' placeholder='New York'></input>
+          <input name='date' placeholder='september' ></input>
+          <input name='count' placeholder='adults' ></input>
+          <input name='search' placeholder='Search'  ></input>
+        </div>
       </header>
     </div>
   );
 }
 
 export default App;
+
